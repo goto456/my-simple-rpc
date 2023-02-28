964912b0968fade45dacf80a384deb0b7e185390
goto456 <goto456@126.com>
2023-02-28 20:07:11 +0800

    init a springboot project.

---
+++ src/main/java/com/goto456/rpc/MySimpleRpcApplication.java
@@ -0,0 +1,13 @@
+package com.goto456.rpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySimpleRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySimpleRpcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goto456/rpc/MySimpleRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goto456.rpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySimpleRpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
